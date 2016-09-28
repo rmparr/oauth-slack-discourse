c16a8db43d87090112fac0aeb0a544d0c3600e8e
yaroslavh <yaroslav.polatayko@gmail.com>
2016-09-28 12:58:59 +0300

    upd

--- plugin.rb
+++ plugin.rb
@@ -58,8 +58,9 @@ class SlackAuthenticator < ::Auth::OAuth2Authenticator
   end
   
   def register_middleware(omniauth)
-    # omniauth.provider :slack, CLIENT_ID, CLIENT_SECRET, scope: 'identity.basic', team: TEAM_ID
-    omniauth.provider :slack, CLIENT_ID, CLIENT_SECRET, scope: 'identity.basic, identify, team:read, users:read', team: TEAM_ID
+    # omniauth.provider :slack, CLIENT_ID, CLIENT_SECRET, scope: 'identity.basic, identify, team:read, users:read', team: TEAM_ID
+    omniauth.provider :slack, CLIENT_ID, CLIENT_SECRET, scope: 'identity.basic', name: :sign_in_with_slack, team: TEAM_ID
+    omniauth.provider :slack, CLIENT_ID, CLIENT_SECRET, scope: 'identify, team:read, users:read', provider_ignores_state: true, team: TEAM_ID
     # omniauth.provider :slack, CLIENT_ID, CLIENT_SECRET, scope: 'identify, users:read', team: TEAM_ID
   end
 end
